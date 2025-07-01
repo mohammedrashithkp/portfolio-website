8736815bd1976870e3e9544cb0956873ed18f76b
mohammedrashithkp <mohammedrashithkp@gmail.com>
2025-07-01 07:53:10 +0530

    Fixed Title and Favicons

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,7 +5,7 @@ const projects = [
         id: 1,
         title: "Portfolio Website",
         description: "Developed and deployed a personal portfolio using Hugo/Vite.js and Tailwind CSS.",
-        githubUrl: "https://github.com/mohammedrashithkp/PORTFOLIO-WEBSITE",
+        githubUrl: "https://github.com/mohammedrashithkp/portfolio-website",
         DemoUrl: "#",
         image: "/projects/Portfolio-Website.png",
         tags: ["HTML", "CSS", "Vite", "Tailwind"]
@@ -118,23 +118,23 @@ export const ProjectsSection = () => {
 
                             </div>
 
-                        
-                        <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
-                        <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
-                        <div className="flex justify-between items-center">
-                            <div className="flex space-x-3">
-                                <a href={project.DemoUrl}
-                                    target="_blank"
-                                    className="text-foreground/80 hover:text-primary transition-colors duration-300">
-                                    <ExternalLink size={20}/>
-                                </a>
-                                <a href={project.githubUrl}
-                                    target="_blank"
-                                    className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                                >
-                                    <Github size={20}/>
-                                </a>
-                            </div>
+
+                            <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
+                            <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
+                            <div className="flex justify-between items-center">
+                                <div className="flex space-x-3">
+                                    <a href={project.DemoUrl}
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                        <ExternalLink size={20} />
+                                    </a>
+                                    <a href={project.githubUrl}
+                                        target="_blank"
+                                        className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                    >
+                                        <Github size={20} />
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -146,11 +146,11 @@ export const ProjectsSection = () => {
             </div>
 
             <div className="text-center mt-12">
-<a className="cosmic-button w-fit flex items-center mx-auto gap-2" 
-target="_blank"
-href="https://www.github.com/mohammedrashithkp">
-    Check My Github <ArrowRight size={16}/>
-</a>
+                <a className="cosmic-button w-fit flex items-center mx-auto gap-2"
+                    target="_blank"
+                    href="https://www.github.com/mohammedrashithkp">
+                    Check My Github <ArrowRight size={16} />
+                </a>
             </div>
         </div>
 
